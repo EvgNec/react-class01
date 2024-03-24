3241b21a13495b5a33fc5c819f72f99b12ca5e9a
Evgeniy <evgnec@gmail.com>
2024-03-24 12:08:59 +0200

    upd

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,9 +1,10 @@
 import css from "./EventCard.module.css";
 import PropTypes from "prop-types";
 import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from "react-icons/fa";
-import { formatEventStart } from '../../FormatEventStart';
+import { formatEventStart } from '../../utils/formatEventStart';
 
 export const EventCard = ({ name, location, speaker, type, start, end }) => {
+    const formattedStart = formatEventStart(start);
   return (
     <div className={css.event}>
       <h2 className={css.title}>{name}</h2>
@@ -17,7 +18,7 @@ export const EventCard = ({ name, location, speaker, type, start, end }) => {
       </p>
       <p className={css.info}>
         <FaCalendarAlt />
-        {start}
+        {formattedStart}
       </p>
       <p className={css.info}>
         <FaClock />
