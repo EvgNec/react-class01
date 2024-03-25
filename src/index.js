2aae2ad18a6a54464bea72aeeb4715f0e4757eef
Evgeniy <evgnec@gmail.com>
2024-03-25 20:19:59 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './components/App/App';
-import { ThemeProvider } from '@emotion/react';
-import './index.css';
-import { theme } from './constans/theme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App/App";
+import { ThemeProvider } from "@emotion/react";
+import "./index.css";
+import { theme } from "./constans/theme";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <ThemeProvider theme={theme}>
       <App />
-      </ThemeProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
